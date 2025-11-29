1b7660d5cc1d54cdb098b4e8e58f5e323931549e
Arkin26 <arkinutmal@gmail.com>
2025-11-29 22:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -181,4 +181,6 @@ def start_discord():
 
 if __name__ == "__main__":
     threading.Thread(target=start_discord).start()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
